1348d8c1fd196581bdee60d3a675bf7a2622fb54
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-02-03 14:31:28 +0700

    create project & add dependencies

---
+++ src/main/java/rifqimuhammadaziz/todolist/App.java
@@ -0,0 +1,13 @@
+package rifqimuhammadaziz.todolist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
